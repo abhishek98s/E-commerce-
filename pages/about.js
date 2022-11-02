cecce43036fd4b39b181eb52df1ffc5ac4848106
Abhishek <abhishekshakya98.com@gmail.com>
2022-11-02 17:45:08 +0545

    removed aboutus

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,9 @@
-export const getStaticProps = async () => {
-    const res = await fetch('http://localhost:3000/api/hello');
-    const data = await res.json();
-    console.log(data)
-    return{
-        props: { user: data} 
-    }
-}
-
 function About({user}){
-    console.log(user)
     return(
     <>  
-        <p>dsd</p>
-        {user.map((use) => (
-            <div key={use.id}>
-            <a>
-                <li>{use.name}</li>
-            </a>  
-            </div>  
-        ))}
-    </>)
+        <p>About us</p>
+    </>
+    )
 }
 
 export default About;
